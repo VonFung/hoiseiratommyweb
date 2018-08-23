af0087e41181da30f52b34067beb21a82c048771
VonFung <41778370+VonFung@users.noreply.github.com>
2018-08-23 22:02:17 +0900

    Update GetVote.php

--- db_access/GetVote.php
+++ db_access/GetVote.php
@@ -1,8 +1,9 @@
 <?php
   $database = include('config.php');
 
-  $conn = mysql_connect($database['host'], $database['username'], $database['password'], $database['database']) or die('Error with MySQL connection');
+  $conn = mysql_connect($database['host'], $database['username'], $database['password']) or die('Error with MySQL connection');
   mysql_query("SET NAMES 'utf8'");
+  mysql_select_db($database['database']);
   $sql = "SELECT * FROM musiclist WHERE CODE LIKE '%OR%'";
   $result = mysql_query($sql) or die('MySQL query error');
   $rows = array();
