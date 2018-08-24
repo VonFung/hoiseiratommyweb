83a487c297e9c88474f1c297db609e9036efda01
VonFung <41778370+VonFung@users.noreply.github.com>
2018-08-24 11:02:55 +0900

    Update GetVote.php

--- db_access/GetVote.php
+++ db_access/GetVote.php
@@ -1,10 +1,15 @@
 <?php
   $database = include('config.php');
+  $expire = $_POST["expire"];
 
   $conn = mysql_connect($database['host'], $database['username'], $database['password']) or die('Error with MySQL connection');
   mysql_query("SET NAMES 'utf8'");
   mysql_select_db($database['database']);
-  $sql = "SELECT * FROM musiclist WHERE CODE LIKE '%OR%'";
+  $sql = "SELECT id, TITLE, DESCRIPTION FROM vote WHERE HIDED = FALSE";
+  if(!$expire) {
+    $sql += " AND EXPIRE_DATE >= DATE(CURDATE())";
+  }
+  $sql += " ORDER BY ID DESC";
   $result = mysql_query($sql) or die('MySQL query error');
   $rows = array();
   while($r = mysql_fetch_assoc($result)) {
